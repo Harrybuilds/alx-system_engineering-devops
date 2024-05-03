85b0b47fe672cbe62a114d8928fb38f29661e963
Harrison <harrisonchialuka@gmail.com>
2024-05-03 14:37:18 +0100

    task 1 of configuration management project

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #puppet manifest to install python's flask package using pip
 
 exec { 'install-flask':
-  command => '/usr/bin/pip3 install Flask=2.1.0',
+  command => '/usr/bin/pip3 install Flask==2.1.0',
   unless  => '/usr/bin/pip3 show Flask | grep Version | grep q- 2.1.0',
 }
 
