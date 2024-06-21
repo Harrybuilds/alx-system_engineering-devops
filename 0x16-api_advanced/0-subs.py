cef4a4c0e6f3190ce1099e2e0decbc60f8154497
Harrison <harrisonchialuka@gmail.com>
2024-06-21 13:47:27 +0100

    correction 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,12 +7,12 @@ of all subscribers of a sub-reddit using the reddit API
 import requests
 
 
-url = f'http://www.reddit.com/r/{subreddit}/about.json'
-headers = {'User-Agent': 'custom agent'}
-
-
 def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit"""
+
+    url = f'http://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'custom agent'}
+
     if subreddit is None or type(subreddit) is not str:
         return 0
     r = requests.get(url, headers=headers).json()
