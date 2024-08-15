18799872d2e9b5d0726b8dcca2568a55081f8387
Harrison <harrisonchialuka@gmail.com>
2024-08-15 07:27:51 +0100

    task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,10 +7,14 @@ def recurse(subreddit, hot_list=[]):
     """returning top ten post titles recursively"""
     global after
     user_agent = {'User-Agent': 'api_advanced-project'}
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    parameters = {'after': after}
-    results = requests.get(url, params=parameters, headers=user_agent,
-                           allow_redirects=False)
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json")
+
+    results = requests.get(
+        url, params={'after': after},
+        headers=user_agent,
+        allow_redirects=False
+    )
 
     if results.status_code == 200:
         after_data = results.json().get("data").get("after")
